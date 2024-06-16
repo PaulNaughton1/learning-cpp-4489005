a7d57046399110d6148d7757b37272211b374cd0
PaulNaughton1 <paulnaughton2001@gmail.com>
2024-06-16 16:24:37 +0000

    chapter 1 and 2b

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -7,9 +7,9 @@
 
 int main(){
     std::string str;
+    std::cout << "What is your name?" << '\n';
     std::cin >> str;
-    std::cout << str;
 
-    std::cout << std::endl << std::endl;
-    return (0);
+    std::cout << "Hello, " << str << "!";
+    return 0;
 }
--- src/Ch02/02_03b/CodeDemo.cpp
+++ src/Ch02/02_03b/CodeDemo.cpp
@@ -3,10 +3,24 @@
 // Using Variables, by Eduardo Corpeño 
 
 #include <iostream>
+#define cout std::cout
+#define endln std::endl
+
+int a = 4, b = 5;
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
-    
-    std::cout << std::endl << std::endl;
+    bool myFlag;
+    myFlag = false;
+
+    cout << "myFlag = " << myFlag << std::endl;
+    cout << "a = " << a << endln;
+    cout << "b = " << b << std::endl;
+    cout << "a + b = " << a+b << std::endl;
+    cout << "a - b = " << a-b << std::endl;
+    cout << std::endl << std::endl;
+
+    unsigned int positive; // an unsigned variable will take the binary representation of -2 so instead of -2 it is 2^32 - 2
+    positive = a-b;
+    cout << positive <<endln;
     return (0);
 }
